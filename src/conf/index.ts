41c969d6cbaa826893f9577fe74375e2024146ba
PRASSamin <prassamin@gmail.com>
2026-07-06 05:56:27 +0600

    fix: build error

--- src/conf/index.ts
+++ src/conf/index.ts
@@ -1,17 +1,5 @@
-// Tiktok Configurations
-export const ENABLE_TIKTOK = true;
-// Instagram Configurations
-export const ENABLE_INSTAGRAM = true;
-// Facebook Configurations
-export const ENABLE_FACEBOOK = true;
-// Youtube Configurations
-export const ENABLE_YOUTUBE = false;
-
 // LOGGER
 export const DOWNLOADER_SESSION_TTL = 120; // 2 minutes
-
-export const SEND_TO_DISCORD = false; // TODO: Enable Discord Logger
-export const ENABLE_LOGGER = false; // TODO: Enable Logger
 
 export const LOGGER_CACHE_TTL = 60 * 10; // 10 minutes
 
